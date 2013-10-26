172ab40acf7cdb4bb66a6f5ecc7e7f48fe256bf5
Sudheer K. Parasker <sparasker@gmail.com>
2013-10-26 10:49:19 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moblaunch::Application.config.secret_key_base = '940c79aaaf6f529d9bdbebbe5953b3c05282d3308f8ce73c8c9bbb936973d0f3121989a2d9a39f6a4f91dd1af7beb1444b8879471a563361b50ba57831c51bc7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Moblaunch::Application.config.session_store :cookie_store, key: '_moblaunch_session'
